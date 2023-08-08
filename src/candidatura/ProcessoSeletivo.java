bd2f7e9f377413744367eec49ea4469cc3f235b5
killuaasura <wellortega@gmail.com>
2023-08-08 01:03:58 -0300

    criando metodos e laços

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,31 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2000.0);
-        analisarCandidato(2100.0);
+
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+        int candidatosSelecionados=0;
+        int candidatosAtual=0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2000);
     }
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
